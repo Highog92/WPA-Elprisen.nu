fde144e10b24e995e68cc144db6fbb3dd307e620
Highog92 <Madsserritslev@gmail.com>
2023-11-08 09:22:37 +0100

    Update

--- Elpriser.nu/Javascript/Getdate.js
+++
@@ -1,8 +0,0 @@
-function updateDate(e) {
-    console.log("updateDate", e);
-        let selectedDate = e.target.value
-        // console.log("Dato update",selectedDate);
-        
-        document.getElementById("displayDate").textContent = selectedDate;
-        document.getElementById("displaySelectedDate").textContent = selectedDate;
-    }
--- Elpriser.nu/Javascript/Historik.js
+++ Elpriser.nu/Javascript/Historik.js
@@ -63,12 +63,13 @@ async function getHistoryData(year, month, day) {
     let data = await useFetch
         (`https://www.elprisenligenu.dk/api/v1/prices/${year}/${month}-${day}_DK1.json`)
 
+        // Dette tømmer .historyBars for info og udfylder den med nyt data når man vælger en ny dato
     let history = document.querySelector('.historyBars')
     history.innerHTML = ` `
     
     data.map(item => {
         history.innerHTML += `
-        <div class="historyBars">
+        <div class="historydata">
         <p>
            kl. ${item.time_start.slice(11, 16)}
         </p>
